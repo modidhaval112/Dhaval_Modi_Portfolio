5c220d80aff8a797282b07e7a3ab3ca7333cf3d3
Dhaval Modi <dhavalmodi556@gmail.com>
2019-06-30 10:37:32 -0400

    minor change

--- src/App.js
+++ src/App.js
@@ -14,11 +14,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
         <link rel="icon" href="img/favicon.png" type="image/png" />
         <title>Dhaval Modi</title>
         {/* Bootstrap CSS */}
