a3f5fb44f4ae4e538fa59333161bfce9027a0bf5
Dhaval Modi <dhavalmodi556@gmail.com>
2019-07-01 16:53:55 -0400

    Updates

--- precache-manifest.08d9c8ae045ea1f7d5b7ba5c73391eeb.js
+++ precache-manifest.08d9c8ae045ea1f7d5b7ba5c73391eeb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "63f1f2ef5e8b5a19e9a63544eb072a06",
+    "revision": "98ffbb24c4fc9db109a69818e01cc574",
     "url": "/Dhaval_Modi_Portfolio/index.html"
   },
   {
-    "revision": "1e088ef0d70f5eb1e37e",
+    "revision": "53728296a5a830ebe97c",
     "url": "/Dhaval_Modi_Portfolio/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "9eee67cf782a38d35aeb",
-    "url": "/Dhaval_Modi_Portfolio/static/js/2.d6a534e5.chunk.js"
+    "revision": "6edea4347baf3b9b3c55",
+    "url": "/Dhaval_Modi_Portfolio/static/js/2.227b024c.chunk.js"
   },
   {
-    "revision": "1e088ef0d70f5eb1e37e",
-    "url": "/Dhaval_Modi_Portfolio/static/js/main.b5a7e6aa.chunk.js"
+    "revision": "53728296a5a830ebe97c",
+    "url": "/Dhaval_Modi_Portfolio/static/js/main.d4160720.chunk.js"
   },
   {
     "revision": "078daa35762ce7bee2b2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Dhaval_Modi_Portfolio/precache-manifest.e8c2a779dae7c77ffa3dcd2bb506d9ac.js"
+  "/Dhaval_Modi_Portfolio/precache-manifest.08d9c8ae045ea1f7d5b7ba5c73391eeb.js"
 );
 
 self.addEventListener('message', (event) => {
